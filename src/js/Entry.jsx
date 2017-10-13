41bc7b84659a4487250921348038d11f3219297b
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2017-10-13 07:33:45 -0700

    Creates our first component using React.createElement

--- src/js/Entry.jsx
+++ src/js/Entry.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
 
 import '../scss/app.scss'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+/*
+Before there was JSX, there was/is React.createElement(). This method is the foundation of React.
+
+The reason we start here is so that we clearly understand that React components are functions,
+and a React app is just a composition of functions all the way down.
+
+React.createElement takes three arguments, a HTML element type, a props object,
+and then any number of children. We can return a string like we are here,
+or an array of nested createElement calls.
+*/
+
+const ourFirstComponent = React.createElement('h1', null, 'React 101')
+
+ReactDOM.render(ourFirstComponent, document.getElementById('app'))
