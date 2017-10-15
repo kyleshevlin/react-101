d4ce9d04e57d918bc65d650d6ad804ffcbd53239
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2017-10-15 12:51:34 -0700

    Recreates our first basic app using JSX instead of React.createElement

--- src/js/Entry.jsx
+++ src/js/Entry.jsx
@@ -3,44 +3,37 @@ import ReactDOM from 'react-dom'
 
 import '../scss/app.scss'
 
-/*
-Because the markup of React components is entirely function calls,
-we can use some interesting techniques to modify these functions,
-like memoization, or as I'm about to show you, currying & partial application.
+// JSX is the most common way React markup is written. JSX is a combination of
+// JavaScript and XML. It looks a lot like writing HTML with just a few quirks
+// to get used to (like `className` and `htmlFor`). It is a very expressive way
+// of writing markup, allowing you to express JavaScript directly in your markup.
 
-So we don't repeat ourselves so much, we're going to make React.createElement
-a curried function. That means we will change it from a function that takes several
-arguments, to a series of several functions that each take one argument. Given the same
-arguments, they will return identical results, but currying will allow us to partially
-apply values, baking in arguments that aren't used until all arguments are received.
+// I want it to be very clear that JSX is simply syntactic sugar for calls to
+// React.createElement. To demonstrate this, I will keep our previous markup
+// in place, but commented out.
 
-For example, the parapgraphs below have the same first two arguments, let's tidy
-this up a bit.
-*/
+// const ce = React.createElement
 
-const ce = React.createElement
-
-const curriedCe = tag => props => children => ce(tag, props, children)
-
-const p = curriedCe('p')(null)
+// const ourFirstBasicApp = (
+//   ce('div', { className: 'wrap' }, [
+//     ce('h1', null, 'React 101'),
+//     ce('h2', null, 'by Kyle Shevlin'),
+//     ce('article', { className: 'article' }, [
+//       ce('p', null, 'How excited are you to be learning React?!'),
+//       ce('p', null, 'I know I am excited to be teaching it!')
+//     ])
+//   ])
+// )
 
 const ourFirstBasicApp = (
-  ce('div', { className: 'wrap' }, [
-    ce('h1', null, 'React 101'),
-    ce('h2', null, 'by Kyle Shevlin'),
-    ce('article', { className: 'article' }, [
-      p('How excited are you to be learning React?!'),
-      p('I know I am excited to be teaching it!')
-    ])
-  ])
+  <div className="wrap">
+    <h1>React 101</h1>
+    <h2>by Kyle Shevlin</h2>
+    <article className="article">
+      <p>How excited are you to be learning React?!</p>
+      <p>I know I am excited to be teaching it!</p>
+    </article>
+  </div>
 )
-
-/*
-You can imagine creating all sorts of partially applied functions
-to build your app this way. But most React apps do not use React.createElement
-this way. Instead, they use JSX, a syntactic sugar for React.createElement calls.
-We'll start using JSX in the next commit, but it was impprtant to understand
-what its doing under the hood before we started using it.
-*/
 
 ReactDOM.render(ourFirstBasicApp, document.getElementById('app'))
