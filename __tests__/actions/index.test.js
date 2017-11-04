19e8b3c902a0a0fcb85db91e19da2f9c0a079565
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2017-11-03 21:13:28 -0700

    write tests for actionCreators and reducer

--- __tests__/actions/index.test.js
+++ __tests__/actions/index.test.js
@@ -1,10 +1,66 @@
 import * as actions from '../../src/js/actions'
+import * as types from '../../src/js/constants/actionTypes'
 
 describe('Actions', () => {
-  it('testActionCreator should return a POJO with type TEST_ACTION_CREATOR', () => {
-    const action = actions.testActionCreator()
+  it('requestUserProfile should return the correct type and username', () => {
+    const username = 'Kyle'
+    const action = actions.requestUserProfile(username)
+    const expected = {
+      type: types.REQUEST_USER_PROFILE,
+      username
+    }
 
-    expect(typeof action).toBe('object')
-    expect(action.type).toBe('TEST_ACTION_CREATOR')
+    expect(action).toEqual(expected)
+  })
+
+  it('receiveUserProfileSuccess should return the correct type and user', () => {
+    const user = { name: 'Kyle' }
+    const action = actions.receiveUserProfileSuccess(user)
+    const expected = {
+      type: types.RECEIVE_USER_PROFILE_SUCCESS,
+      user
+    }
+
+    expect(action).toEqual(expected)
+  })
+
+  it('receiveUserProfileFail should return the correct type and error', () => {
+    const error = { message: 'error' }
+    const action = actions.receiveUserProfileFail(error)
+    const expected = {
+      type: types.RECEIVE_USER_PROFILE_FAIL,
+      error
+    }
+
+    expect(action).toEqual(expected)
+  })
+
+  it('requestUserRepos should return the correct type', () => {
+    const action = actions.requestUserRepos()
+    const expected = { type: types.REQUEST_USER_REPOS }
+
+    expect(action).toEqual(expected)
+  })
+
+  it('receiveUserReposSuccess should return the correct type and repos', () => {
+    const repos = ['foo', 'bar']
+    const action = actions.receiveUserReposSuccess(repos)
+    const expected = {
+      type: types.RECEIVE_USER_REPOS_SUCCESS,
+      repos
+    }
+
+    expect(action).toEqual(expected)
+  })
+
+  it('receiveUserReposFail should return the correct type and error', () => {
+    const error = { message: 'error' }
+    const action = actions.receiveUserReposFail(error)
+    const expected = {
+      type: types.RECEIVE_USER_REPOS_FAIL,
+      error
+    }
+
+    expect(action).toEqual(expected)
   })
 })
--- __tests__/reducers/index.test.js
+++ __tests__/reducers/index.test.js
@@ -3,30 +3,97 @@ import * as actions from '../../src/js/actions'
 
 describe('Reducer', () => {
   it('returns state when action is unaccounted for', () => {
-    const initialState = {
+    const state = {
       testState: false
     }
+    const action = { type: 'UNACCOUNTED_ACTION' }
 
-    expect(reducer(
-      initialState,
-      { type: 'UNACCOUNTED_ACTION' }
-    )).toEqual(initialState)
+    expect(reducer(state, action)).toEqual(state)
   })
 
-
-  it('toggles testState with action TEST_ACTION_CREATOR', () => {
-    const initialState = {
-      testState: false
+  it('requestUserProfile updates waitingForUser, hasUserError and userError', () => {
+    const state = {
+      waitingForUser: false,
+      hasUserError: true,
+      userError: { message: 'foo' }
     }
+    const action = actions.requestUserProfile('foo')
+    const nextState = reducer(state, action)
 
-    const expectedState = {
-      testState: true
+    expect(nextState.waitingForUser).toBe(true)
+    expect(nextState.hasUserError).toBe(false)
+    expect(nextState.userError).toBeNull()
+  })
+
+  it('receiveUserProfileSuccess updates waitingForUser and user', () => {
+    const state = {
+      waitingForUser: true,
+      user: null
     }
+    const user = { name: 'Kyle' }
+    const action = actions.receiveUserProfileSuccess(user)
+    const nextState = reducer(state, action)
 
-    const action = actions.testActionCreator()
+    expect(nextState.waitingForUser).toBe(false)
+    expect(nextState.user).toEqual(user)
+  })
 
-    expect(
-      reducer(initialState, action)
-    ).toEqual(expectedState)
+  it('receiveUserProfileFail updates waitingForUser and error', () => {
+    const state = {
+      waitingForUser: true,
+      hasUserError: false,
+      userError: null
+    }
+    const error = { message: 'foo' }
+    const action = actions.receiveUserProfileFail(error)
+    const nextState = reducer(state, action)
+
+    expect(nextState.waitingForUser).toBe(false)
+    expect(nextState.hasUserError).toBe(true)
+    expect(nextState.userError).toEqual(error)
+  })
+
+  it('requestUserRepos updates waitingForRepos, hasReposError, and reposError', () => {
+    const state = {
+      waitingForRepos: false,
+      hasReposError: true,
+      reposError: { message: 'foo' }
+    }
+    const action = actions.requestUserRepos()
+    const nextState = reducer(state, action)
+
+    expect(nextState.waitingForRepos).toBe(true)
+    expect(nextState.hasReposError).toBe(false)
+    expect(nextState.reposError).toBeNull()
+  })
+
+  it('receiveUserReposSuccess updates waitingForRepos and repos', () => {
+    const state = {
+      waitingForRepos: true,
+      user: {
+        repos: []
+      }
+    }
+    const repos = ['foo', 'bar']
+    const action = actions.receiveUserReposSuccess(repos)
+    const nextState = reducer(state, action)
+
+    expect(nextState.waitingForRepos).toBe(false)
+    expect(nextState.user.repos).toEqual(repos)
+  })
+
+  it('receiveUserReposFail updates waitingForRepos and error', () => {
+    const state = {
+      waitingForRepos: true,
+      hasReposError: false,
+      reposError: null
+    }
+    const error = { message: 'foo' }
+    const action = actions.receiveUserReposFail(error)
+    const nextState = reducer(state, action)
+
+    expect(nextState.waitingForRepos).toBe(false)
+    expect(nextState.hasReposError).toBe(true)
+    expect(nextState.reposError).toEqual(error)
   })
 })
